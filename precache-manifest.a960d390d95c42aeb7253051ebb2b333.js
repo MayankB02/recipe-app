a03a65032de6ccd592c256fb873cbaec9e73053c
Mayank <m.bubna19@gmail.com>
2020-04-10 20:46:35 +0530

    Updates

---
+++ precache-manifest.a960d390d95c42aeb7253051ebb2b333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d349ab50e6af36d11ae1b4d4488af39",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "db9390906bdaba21be94",
+    "url": "/recipe-app/static/css/main.da442382.chunk.css"
+  },
+  {
+    "revision": "bb4ca0c93ef2dc26297a",
+    "url": "/recipe-app/static/js/2.a900f031.chunk.js"
+  },
+  {
+    "revision": "db9390906bdaba21be94",
+    "url": "/recipe-app/static/js/main.d40b6a1c.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
